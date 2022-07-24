80fcb4f709d2b6b53818aa756b7fddeaaa9a7902
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-07-24 10:07:07 +0200

    add fix to task

--- test.sh
+++ test.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-#
-#This script is game "Number"
 
 echo -e "\n NumberJack \n"
 ch=0
 
 while [ $ch -ne 3 ]; do
   echo  "
-    PLAY : Hit 1 and enter.
-    HELP : Hit 2 and enter.
-    EXIT : Hit 3 and enter.
-    "
-  read -p -r "Enter your choice : " ch
+  PLAY : Hit 1 and enter.
+  HELP : Hit 2 and enter.
+  EXIT : Hit 3 and enter.
+  "
+read -rp "Enter your choice : " ch
+
   if [[ $ch -eq 1 ]]; then
-    x=0
-    c=0
-    p=0
-    read -p -r "Enter any number between 0 and 9 : " n
+    x=0 ;c=0 ;p=0
+    read -rp "Enter any number between 0 and 9 : " n
+
     while [ $c -eq 0 ]; do
-      x=11
-      r=( "$(shuf -i 0-9 -n 10)" )
-      echo "${r[*]} "
+      x=11;
+      #r=($(shuf -i 0-9 -n 10))
+      mapfile -t r < <(shuf -i 0-9 -n 10)
+      
+      echo "${r[@]}" " "
+
       for i in {1..10}; do
         a[$i]=$i
       done
-      echo "${a[*]} "
-      read -t 5 -p -r "Enter the index of your number : " x
 
+      echo "${a[@]}"
+
+      read -r -t 5 -p "Enter the index of your number : " x
+      
       if [[ $? -gt 128 ]]; then
         c=1
         break
@@ -34,13 +37,13 @@ while [ $ch -ne 3 ]; do
 
       if [[ ${r[$((x))-1]} -eq $n ]]; then
         echo "Great"
-        (( p=p+1 ))
+        ((p++))
       else
         c=1
         break
       fi
     done
-
+    
   elif [[ $ch -eq 2 ]]; then
     echo "HELP: INSTRUCTIONS TO PLAY THE GAME. "
   else
